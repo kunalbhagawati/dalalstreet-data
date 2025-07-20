42f28b6f26237762fdd78ab627c89b9371f2104e
knl.bhg@gmail.com <knl.bhg@gmail.com>
2025-07-20 13:06:36 +0530

    Consolidate project version into 1 file

--- dalalstreet_data/__init__.py
+++ dalalstreet_data/__init__.py
@@ -1,1 +1,12 @@
-__version__ = "1.0.0"
+try:
+    from importlib import metadata
+except ImportError:
+    # Python < 3.8
+    import importlib_metadata as metadata # type: ignore
+
+try:
+    __version__ = metadata.version("dalalstreet-data")
+except metadata.PackageNotFoundError:
+    # package is not installed
+    __version__ = "unknown"
+
